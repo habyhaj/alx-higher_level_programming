c14e775367275c048828e6d1a70047dc5e4b336e
habyhaj <habyhaj07@gmail.com>
2023-07-16 17:25:02 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if (len(tuple_a) == 0):
+        tuple_a = (0, 0)
+    elif (len(tuple_a) == 1):
+        tuple_a = (tuple_a[0], 0)
+
+        if (len(tuple_b) == 0):
+            tuple_b = (0, 0)
+        elif (len(tuple_b) == 1):
+            tuple_b = (tuple_b[0], 0)
+
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
